9ddb7bab9cf15150310bfb4e2384acbc6ee0c6b6
LegendisM <makenkap@gmail.com>
2022-07-16 15:59:34 +0430

    update login widget

--- lib/authentication/widgets/login.dart
+++ lib/authentication/widgets/login.dart
@@ -10,8 +10,52 @@ class Login extends StatefulWidget {
 class _Login extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: const [Text("222222")]);
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(25, 15, 25, 20),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.start, children: const [
+          IconButton(
+              onPressed: null,
+              icon: IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.login_outlined,
+                    size: 60.0,
+                  ))),
+          SizedBox(
+            height: 35,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                label: Text("Username"),
+                icon: Icon(Icons.verified_user),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromARGB(255, 39, 39, 39), width: 1))),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                label: Text("Phone"),
+                icon: Icon(Icons.phone),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromARGB(255, 39, 39, 39), width: 1))),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          TextField(
+              decoration: InputDecoration(
+            label: Text("Password"),
+            icon: Icon(Icons.password),
+            enabledBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 39, 39, 39), width: 1),
+            ),
+          ))
+        ]));
   }
 }
